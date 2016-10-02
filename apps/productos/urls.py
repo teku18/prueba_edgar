0859df4c4b9e168ded0f7f726ae8235e24eea1ae
edgar <edgar94_ut@outlook.com>
2016-10-01 22:06:53 -0500

    ya actualiza, elimina y muestra el detalle de los productos

--- apps/productos/urls.py
+++ apps/productos/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import index, registrar_producto, actualizar_producto, lista_productos
+from .views import index, registrar_producto, actualizar_producto, lista_productos, eliminar_producto, detalle_producto
 
 urlpatterns = [
 
@@ -8,4 +8,8 @@ urlpatterns = [
     url(r'^actualizarProducto/(?P<pk>\d+)', actualizar_producto.as_view(), name='actualizar-producto'),
 
     url(r'^listaProducto$', lista_productos.as_view(), name='lista-productos'),
+
+    url(r'^eliminarProducto/(?P<pk>\d+)', eliminar_producto.as_view(), name='eliminar-producto'),
+
+    url(r'^detalleProducto/(?P<pk>\d+)', detalle_producto.as_view(), name='detalle-producto'),
 ]
--- apps/productos/views.py
+++ apps/productos/views.py
@@ -1,5 +1,9 @@
-from django.views.generic import TemplateView, CreateView, UpdateView, ListView
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.views.generic import TemplateView, CreateView, UpdateView, ListView, FormView, DeleteView, DetailView
 from .models import producto
+from .forms import productosForm
+from django.core.urlresolvers import reverse_lazy
 
 # Create your views here.
 
@@ -12,29 +16,49 @@ class index(TemplateView):
         return context
 
 
-class registrar_producto(CreateView):
-    model = producto
+class registrar_producto(FormView):
+    # model = producto
+    form_class = productosForm
     template_name = 'registro_productos.html'
+    success_url = reverse_lazy('inicio')
 
-#REDIRECCIONA A LA MISMA PAGINA EN LA QUE ESTA PACICIONADO
-    def get_success_url(self):
-        return self.request.path
-
-    fields = ('nombre', 'descripcion',)
+    def form_valid(self, form):
+        form.save()
+        print('se guadro correctamente todo')
+        return super(registrar_producto, self).form_valid(form)
+#
+# #REDIRECCIONA A LA MISMA PAGINA EN LA QUE ESTA POCICIONADO
+#     def get_success_url(self):
+#         return self.request.path
 
 
 class actualizar_producto(UpdateView):
     model = producto
     template_name = 'actualizar_producto.html'
-    success_url = '/productos/registrarProducto'
+    form_class =productosForm
+    success_url = '/listaProducto'
 
-    fields = ('nombre', 'descripcion',)
 
 class lista_productos(ListView):
     template_name = 'lista_productos.html'
-    model = producto
+    #model = producto
     context_object_name = 'lista_productos'
+
+    #traigo la lista de los datos con el queryset ya que me permite
+    #traer la lista de los datos ordenados
+    queryset = producto.objects.all().order_by('id')
+
 
     def get_context_data(self, **kwargs):
         context = super(lista_productos, self).get_context_data(**kwargs)
         return context
+
+class eliminar_producto(DeleteView):
+    template_name = 'eliminar_producto.html'
+    model = producto
+    success_url = reverse_lazy('lista-productos')
+
+class detalle_producto(DetailView):
+    template_name = 'detalle_producto.html'
+    model = producto
+    context_object_name = 'detalle_producto'
