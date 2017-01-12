4f7d839cb573cfa21bd566e1555ef6577086ef70
edgar <edgar94_ut@outlook.com>
2017-01-12 13:58:12 -0600

    modificacion setting heroku

--- proyectoEdgar/settings.py
+++ proyectoEdgar/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'proyectoEdgar.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'proyecto_edgar',                      # Or path to database file if using sqlite3.
+        'NAME': 'dafv37kjdht035',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'nqywcnppegmafd',
+        'PASSWORD': '576fd4962cb947ece851955071f0e0007ace5975e71560eb3453f51a993bcc18',
+        'HOST': 'ec2-54-163-224-108.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                      # Set to empty string for default.
     }
 }
